005a8796506fad19df70fbf4e045458f18e6735e
Mudriy Daniil <mudry.daniil@gmail.com>
2026-05-07 21:38:52 +0300

    chore: initial project setup with sass, redux and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import { Title } from './components/Title'
-import BrowserSafari from './assets/icons/browser-safari.svg?react'
-
 export function App() {
-    return (
-        <>
-            <Title>Vite + React + Typescript (manual setup)</Title>
-            <BrowserSafari width={60} height={60} color='red' />
-        </>
-    )
+  return <h1>Pixema Project Ready</h1>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import { App } from './App'
+import './assets/styles/index.scss'
 
-ReactDOM.createRoot(document.querySelector('#app')!).render(
+ReactDOM.createRoot(document.getElementById('app')!).render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode >
-)
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+);
